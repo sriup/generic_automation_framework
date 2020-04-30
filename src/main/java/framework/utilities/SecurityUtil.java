f7b061d07d7520509733615c8df4cb5e4bbc903c
upputuris <sridhar.upputuri@sparksoftcorp.com>
2020-04-30 14:09:13 -0400

    1) Pushing POM.xml 2) Added documentation for FakeDataUtil and SecurityUtil classes.

--- src/main/java/framework/utilities/SecurityUtil.java
+++ src/main/java/framework/utilities/SecurityUtil.java
@@ -20,22 +20,34 @@ public class SecurityUtil {
 	 * 
 	 */
 	public String getToken(String secretKey) {
-		String token = null;
+		// create the instance of the TOTP with the security key
 		Totp otp = new Totp(secretKey);
-		token = otp.now();
-		return token;
-
+		// Get the latest token
+		return otp.now();
 	}
 
+	
+	/**
+	 * Encrypts the text 
+	 * <i>Note : </i> Make sure to add <u>AES_KEY</u> in the User Environment Variables<br>
+	 *  Please use the same AES_KEY across all the team members, otherwise tests will fail. 
+	 * @param textToEncrypt  Text to encrypt
+	 * @return Encrypted text
+	 */
 	public String encrypt(String textToEncrypt) {
 		String encryptedtext = null;
 		try {
+			// Get the AES_KEY from user environment variables
 			String key = System.getenv("AES_KEY");
+			// Create the key
 			SecretKeySpec aesKey = new SecretKeySpec(key.getBytes(), "AES");
+			// Create cipher instance
 			Cipher cipher = Cipher.getInstance("AES");
-
+			// initialize with encryption mode
 			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			// Get the encypted value
 			byte[] encrypted = cipher.doFinal(textToEncrypt.getBytes());
+			// Convert the value from bytes to standard text
 			encryptedtext = DatatypeConverter.printBase64Binary(encrypted);
 			if (!decrypt(encryptedtext).equals(textToEncrypt)) {
 				throw new Exception("unable to decrypt the encrypted text");
@@ -43,24 +55,37 @@ public class SecurityUtil {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// return the encrypted value
 		return encryptedtext;
 
 	}
 
+	/**
+	 * Decrypts the encrypted text 
+	 * <i>Note : </i> Make sure to add <u>AES_KEY</u> in the User Environment Variables<br>
+	 *  Please use the same AES_KEY across all the team members, otherwise tests will fail. 
+	 * @param textToDecrypt  Text to decrypt
+	 * @return Decrypted text
+	 */
 	public String decrypt(String textToDecrypt) {
 		String decrypted = null;
 		try {
-
+			// Get the AES_KEY from user environment variables
 			String key = System.getenv("AES_KEY");
+			// Create the key
 			SecretKeySpec aesKey = new SecretKeySpec(key.getBytes(), "AES");
+			// Create cipher instance
 			Cipher cipher = Cipher.getInstance("AES");
-
+			// initialize with decryption mode
 			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			// Get the decypted value
 			byte[] byteText = DatatypeConverter.parseBase64Binary(textToDecrypt);
+			// Convert the value from bytes to standard text
 			decrypted = new String(cipher.doFinal(byteText));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// return the decrypted value
 		return decrypted;
 	}
 }
