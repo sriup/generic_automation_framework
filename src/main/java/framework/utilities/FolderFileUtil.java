727f6bcaa7af3750adcfc367341d537d2a744045
NageshwarRaoGundla <nageshwar.gundla@sparksoftcorp.com>
2024-07-24 14:00:26 -0400

    1) Implemented clearChromeSiteData 2) Added the increaseNoOfDays method in the DateTimeUtil 3) Implemented the Outlook emails check

--- src/main/java/framework/utilities/FolderFileUtil.java
+++ src/main/java/framework/utilities/FolderFileUtil.java
@@ -237,7 +237,7 @@ public class FolderFileUtil {
 
 		File file = createFile(filePath, filename);
 
-		// Write Content
+		// Write Content.
 		FileWriter writer = new FileWriter(file);
 		
 		int counter = 1;
